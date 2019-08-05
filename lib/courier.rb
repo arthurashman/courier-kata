e1d1b3716bdea46c20b978f861cde2303f70c736
Arthur Ashman <arthurashman@gmail.com>
2019-08-05 17:00:41 +0100

    Refactor print price method to reformat reciept output
    Reciept now prints subtotal, speedy shipping cost and total

--- lib/courier.rb
+++ lib/courier.rb
@@ -1,9 +1,13 @@
 class Courier
+
   def initialize
     @subtotal = 0
+    @speedy = false
   end
 
-  def calculate_price(parcel)
+  def calculate_price(parcel:, speedy: false)
+    @speedy = true if speedy == true
+    
     case parcel.size
     when "small"
       @subtotal = 3
@@ -17,6 +21,13 @@ class Courier
   end
 
   def print_price
-    receipt = "Total = $#{@subtotal}"
-  end
+    receipt = ["Subtotal = $#{@subtotal}"]
+    if @speedy == true
+      receipt << "Speedy shipping = $#{@subtotal * 2 - @subtotal}"
+      receipt << "Total = $#{@subtotal * 2}"
+    else
+      receipt << "Total = $#{@subtotal}"
+    end
+    receipt.join('\n')  end
 end
+
--- spec/courier_spec.rb
+++ spec/courier_spec.rb
@@ -7,22 +7,22 @@ describe Courier do
     it 'calculates correct price for a small parcel' do
       courier = Courier.new
       small_parcel = instance_double("Parcel", :size => "small")  
-      expect(courier.calculate_price(small_parcel)).to eq(3)
+      expect(courier.calculate_price(parcel: small_parcel)).to eq(3)
     end
     it 'calculates correct price for a medium parcel' do
       courier = Courier.new
       medium_parcel = instance_double("Parcel", :size => "medium")  
-      expect(courier.calculate_price(medium_parcel)).to eq(8)
+      expect(courier.calculate_price(parcel: medium_parcel)).to eq(8)
     end
     it 'calculates correct price for a large parcel' do
       courier = Courier.new
       large_parcel = instance_double("Parcel", :size => "large")  
-      expect(courier.calculate_price(large_parcel)).to eq(15)
+      expect(courier.calculate_price(parcel: large_parcel)).to eq(15)
     end
     it 'calculates correct price for an XL parcel' do
       courier = Courier.new
       xl_parcel = instance_double("Parcel", :size => "XL")  
-      expect(courier.calculate_price(xl_parcel)).to eq(25)
+      expect(courier.calculate_price(parcel: xl_parcel)).to eq(25)
     end
   end
 
@@ -30,26 +30,35 @@ describe Courier do
     it 'prints correct price for a small parcel' do
       courier = Courier.new
       small_parcel = instance_double("Parcel", :size => "small")  
-      courier.calculate_price(small_parcel)
-      expect(courier.print_price).to eq("Total = $3")
+      courier.calculate_price(parcel: small_parcel)
+      expect(courier.print_price).to eq("Subtotal = $3\\nTotal = $3")
     end 
     it 'prints correct price for a medium parcel' do
       courier = Courier.new
       medium_parcel = instance_double("Parcel", :size => "medium")  
-      courier.calculate_price(medium_parcel)
-      expect(courier.print_price).to eq("Total = $8")
+      courier.calculate_price(parcel: medium_parcel)
+      expect(courier.print_price).to eq("Subtotal = $8\\nTotal = $8")
     end 
     it 'prints correct price for a large parcel' do
       courier = Courier.new
       large_parcel = instance_double("Parcel", :size => "large")  
-      courier.calculate_price(large_parcel)
-      expect(courier.print_price).to eq("Total = $15")
+      courier.calculate_price(parcel: large_parcel)
+      expect(courier.print_price).to eq("Subtotal = $15\\nTotal = $15")
     end 
     it 'prints correct price for a XL parcel' do
       courier = Courier.new
       xl_parcel = instance_double("Parcel", :size => "XL")  
-      courier.calculate_price(xl_parcel)
-      expect(courier.print_price).to eq("Total = $25")
+      courier.calculate_price(parcel: xl_parcel)
+      expect(courier.print_price).to eq("Subtotal = $25\\nTotal = $25")
     end 
+  
+    it 'prints correct price for speedy shipping small parcel' do
+      courier = Courier.new
+      small_parcel = instance_double("Parcel", :size => "small")  
+      courier.calculate_price(parcel: small_parcel, speedy: true)
+      expect(courier.print_price).to eq("Subtotal = $3\\nSpeedy shipping = $3\\nTotal = $6")
+    end 
+  
+
   end
 end
