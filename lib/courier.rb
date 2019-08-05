183b8bb24e8439035eb5a1d204c54445ff575215
Arthur Ashman <arthurashman@gmail.com>
2019-08-05 16:35:30 +0100

    Add switch statement to handle all parcel sizes
    Returns correct price for any given parcel size

--- lib/courier.rb
+++ lib/courier.rb
@@ -1,5 +1,14 @@
 class Courier
   def calculate_price(parcel)
-    3
+    case parcel.size
+    when "small"
+      3
+    when "medium"
+      8
+    when "large"
+      15
+    when "XL"
+      25
+    end
   end 
 end
--- spec/courier_spec.rb
+++ spec/courier_spec.rb
@@ -10,5 +10,20 @@ describe Courier do
       small_parcel = instance_double("Parcel", :size => "small")  
       expect(courier.calculate_price(small_parcel)).to eq(3)
     end
+    it 'calculates correct price for a medium parcel' do
+      courier = Courier.new
+      medium_parcel = instance_double("Parcel", :size => "medium")  
+      expect(courier.calculate_price(medium_parcel)).to eq(8)
+    end
+    it 'calculates correct price for a large parcel' do
+      courier = Courier.new
+      large_parcel = instance_double("Parcel", :size => "large")  
+      expect(courier.calculate_price(large_parcel)).to eq(15)
+    end
+    it 'calculates correct price for an XL parcel' do
+      courier = Courier.new
+      XL_parcel = instance_double("Parcel", :size => "XL")  
+      expect(courier.calculate_price(XL_parcel)).to eq(25)
+    end
   end
 end
