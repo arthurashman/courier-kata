60f84e98971a49f6cb5cf60111396b2a182de3f7
Arthur Ashman <arthurashman@gmail.com>
2019-08-05 16:15:04 +0100

    Add extra testing to ensure mixed dimensions calculated correctly
    Checked that as soon as one dimension exceeded the size limit the
    size was still calculated correctly

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -19,6 +19,22 @@ describe Parcel do
       parcel = Parcel.new([100,100,100])
       expect(parcel.calculate_size).to eq("XL")
     end
+    it 'calculates correct size for medium parcels with two small dimensions' do
+      parcel = Parcel.new([5,15,5])
+      expect(parcel.calculate_size).to eq("medium")
+    end
+    it 'calculates correct size for large parcels with two small dimensions' do
+      parcel = Parcel.new([75,5,5])
+      expect(parcel.calculate_size).to eq("large")
+    end
+    it 'calculates correct size for XL parcels with two small dimensions' do
+      parcel = Parcel.new([5,5,100])
+      expect(parcel.calculate_size).to eq("XL")
+    end
+    it 'calculates correct size for large parcels with two medium dimensions' do
+      parcel = Parcel.new([20,20,90])
+      expect(parcel.calculate_size).to eq("large")
+    end
 
 
   end
